36e9ecb69f044a6ea78c37ac5c28737bfcb6b938
Lopez, Yolanda (Contractor) <yolanda.lopez@effem.com>
2024-03-26 15:58:56 -0600

    Tarea2.2

---
+++ Seccion2/Tarea22/Tarea22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grados, faren;
+            Console.Write("Ingrese los grados Centigrados:");
+            grados = double.Parse(Console.ReadLine());
+            faren = (1.8 * grados) + 32;
+            Console.WriteLine("Grados Fahrenheit {0} ", faren);
+            
+
+        }
+    }
+}
