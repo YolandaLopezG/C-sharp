d140dab1464935a99cce35bed16ee1a63048af14
Lopez, Yolanda (Contractor) <yolanda.lopez@effem.com>
2024-03-26 15:48:40 -0600

    Tarea2.1

---
+++ Seccion2/Tarea2/Tarea2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer programa que calcula el perimetro de cualquier poligono regular
+
+            double lados, largo, perimetro;
+
+            Console.Write("Ingrese el numero de lados del poligono: ");
+            lados = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la medida: ");
+            largo = double.Parse(Console.ReadLine());
+
+            perimetro = lados * largo;
+
+            Console.WriteLine("El perimetro es: {0} ", perimetro);
+
+
+        }
+    }
+}
